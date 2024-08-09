05ba0e015778dad530af803b9b5a367860e5e4cf
luanvictorms <luanvictorms@gmail.com>
2024-08-08 21:00:04 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,25 +1,59 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import {
+  CalendarIcon,
+  HomeIcon,
+  InstagramIcon,
+  LogInIcon,
+  LogOutIcon,
+} from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constants/search"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarButton = () => {
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader className="text-left">Menu</SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta da Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant={"outline"} className="gap-1 font-bold">
+              <InstagramIcon />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*<Avatar>
           <AvatarImage src="https://plus.unsplash.com/premium_photo-1658527049634-15142565537a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fHww" />
         </Avatar>
 
         <div>
           <p className="font-bold">Luan Victor</p>
           <p className="text-xs">luanvictorms@gmail.com</p>
-        </div>
+        </div>*/}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
